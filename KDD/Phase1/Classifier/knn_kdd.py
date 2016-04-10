d1d5c617a18eaf111cada967513f8e5211dea3ac
Iman Mk <rimanmk@gmail.com>
2016-04-10 15:29:45 -0400

    add knn with rmse

--- KDD/Phase1/Classifier/knn_kdd.py
+++ KDD/Phase1/Classifier/knn_kdd.py
@@ -9,7 +9,8 @@ import numpy as np
 import pandas as pd
 from sklearn import neighbors
 from sklearn import preprocessing, datasets
-
+from sklearn.metrics import mean_squared_error
+import math
 
 # **************************************************************************
 
@@ -35,13 +36,19 @@ encoded_stepName = le_stepName.fit_transform(stepName)
 
 # **************************************************************************
 
-# TODO:
 # create dictionaries for encoded_studentId and encoded_stepName with keys being studentId & stepName
-#  http://stackoverflow.com/questions/209840/map-two-lists-into-a-dictionary-in-python
+
+
+studentId_dict = dict(zip(studentId, encoded_studentId))
+stepName_dict = dict(zip(stepName, encoded_stepName))
+
+# print("studentId_dict: ", studentId_dict)
+# print("stepName_dict: ", stepName_dict)
+
 
 # **************************************************************************
 
-# TODO:
+
 # we need a matrix like this for our X in fit():
 # [ [6  103432]
 #   [6  162908]
@@ -49,6 +56,23 @@ encoded_stepName = le_stepName.fit_transform(stepName)
 #    ... ]
 # each row consists of an element from encoded_studentId and one element from encoded_stepName.
 # dimensions = 809694 x 2
+
+
+print("esn1: ", encoded_stepName[0])
+
+x_array = []
+
+for i in range(len(encoded_studentId)):
+    x_array.append([encoded_studentId[i], encoded_stepName[i]])
+
+# Convert x_array to np matrix
+X = np.matrix(x_array)
+
+# set Y (labels) to be our cfa
+Y = cfa
+
+print("shape of X: ", X.shape)
+
 
 # **************************************************************************
 
@@ -60,27 +84,11 @@ print(le_studentId.inverse_transform([encoded_studentId[2]]))
 
 # **************************************************************************
 
-# TODO:
-# comment out x / delete after creating the right X.
-# create new X based of the encoded values
-X = [encoded_studentId, encoded_stepName]
-
-# **************************************************************************
 
 # Testing:
 print(len(encoded_studentId))
 print(len(encoded_stepName))
 
-# X = np.matrix([encoded_studentId, encoded_stepName])
-X = np.matrix(X)
-
-# xtest = np.matrix(encoded_stepName)
-# print("xtest: ", xtest.reshape(len(xtest), 1))
-
-# **************************************************************************
-
-# set Y (labels) to be our cfa
-Y = cfa
 
 # **************************************************************************
 
@@ -94,13 +102,12 @@ print("Y Shape: ", Y.shape)
 # print("iris target: ", iris.target)
 # print("iris data: ", iris.data)
 
-yo = dataFile[['Anon Student Id', 'Step Name']]
 
 # **************************************************************************
 
 # KNN function:
 
-# TODO:
+
 # define a func as a wrapper for KNN.
 # @param studentId, stepName
 # @return prediction value
@@ -108,11 +115,52 @@ yo = dataFile[['Anon Student Id', 'Step Name']]
 # looks up the keys for input from dict
 # passes the values to KNN function
 
-# knn = neighbors.KNeighborsClassifier()
-# knn.fit(X, Y)
+knn = neighbors.KNeighborsClassifier(n_neighbors=41)
+knn.fit(X, Y)
 
-# print(knn.predict_proba([[6][103432]]))
+
+def predict_cfa(studentId, stepName):
+    encoded_student_id = studentId_dict.get(studentId)
+    print("encoded_student_id: ", encoded_student_id)
+    encoded_step_name = stepName_dict.get(stepName)
+    print("encoded_step_name: ", encoded_step_name)
+    prediction = knn.predict([[encoded_student_id, encoded_step_name]])
+    print("prediction: ", prediction[0])
+
+
+
+
+# Testing:
+# predict_cfa("0BrbPbwCMz", "3(x+2) = 15")
 
 # **************************************************************************
+
+
+# Treat the whole traindata as testdata and calculate RMSE:
+
+def calculate_rmse():
+
+    prediction_array = np.array([])
+    for element in range(len(encoded_studentId)):
+        prediction = knn.predict([[encoded_studentId[element], encoded_stepName[element]]])
+        prediction_array = np.append(prediction_array, prediction[0])
+        print("Prediction for step {:d} = {:d}".format(element, prediction[0]))
+
+    print("prediction_array: ", prediction_array)
+    rmse = math.sqrt(mean_squared_error(Y, prediction_array))
+    print("RMSE = ", rmse)
+
+
+
+calculate_rmse()
+
+
+# **************************************************************************
+
+
+
+
+
+
 
 
